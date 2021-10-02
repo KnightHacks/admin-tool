56b1e2c5c49e804b090b42563e15152da37edaa7
suneettipirneni <suneettipirneni@icloud.com>
2021-10-02 17:20:14 -0400

    clean up some designs

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,10 @@ import React from 'react';
 import './App.scss';
 import HackerTable from './components/hackerTable';
 import { API } from '@knighthacks/hackathon';
+
 const api = new API();
 function App(): JSX.Element {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-          <HackerTable api={api} />
-        </p>
-      </header>
-    </div>
-  );
+  return <HackerTable api={api} />;
 }
 
 export default App;
