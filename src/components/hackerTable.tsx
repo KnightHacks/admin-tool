c674b5ee50470f3626728635d89d65d670cd1f9f
zenith110 <thequake@live.com>
2021-10-17 10:38:08 -0400

    Modified useState to take in an Object instead of arr

--- src/components/hackerTable.tsx
+++ src/components/hackerTable.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { API, HackerData } from '@knighthacks/hackathon';
 import HackerRender from './hacker';
 interface HackerData {
   beginner: boolean;
 }
 export default function HackerTable() {
-  const [hackerResponse, setHacker] = useState({ HackerData: [] });
+  const [hackerResponse, setHacker] = useState(Object);
   useEffect(() => {
     const hackerURL =
       'https://api.knighthacks.org/api/hackers/get_all_hackers/';
@@ -15,7 +16,7 @@ export default function HackerTable() {
       .then((response) => response.json())
       .then((data) => setHacker(data));
   }, []);
-  console.log(hackerResponse);
+  console.log(hackerResponse['hackers']);
   console.log('first object is: ', hackerResponse[0]);
   console.log(typeof hackerResponse[0]);
   return (
