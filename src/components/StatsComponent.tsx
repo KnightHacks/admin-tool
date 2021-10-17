38e33930b982077b5abd63619662f524a85cb501
zenith110 <thequake@live.com>
2021-10-17 19:15:42 -0400

    Optimizing fetch call for stats

--- src/components/StatsComponent.tsx
+++ src/components/StatsComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 interface StatsInterface {
   hackers: number;
   sponsors: number;
@@ -7,29 +7,23 @@ interface StatsInterface {
 export default function StatsComponent(): JSX.Element {
   const [stats, setStats] = useState<StatsInterface>(Object);
   const statsURL = 'https://api.knighthacks.org/api/stats/user_count/';
-  fetch(statsURL, {
-    method: 'GET',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => setStats(data))
-    .catch((err) => {
-      throw new Error(err);
-    });
+  useEffect(() => {
+    fetch(statsURL, {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setStats(data))
+      .catch((err) => {
+        throw new Error(err);
+      });
+  }, []);
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
+    <div>
       <p>Total hackers: {stats.hackers}</p>
-      <p>Total sponsors: {stats.sponsors}</p>
-      <p>Total: {stats.total}</p>
     </div>
   );
 }
