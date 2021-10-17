781ed52810dd8b3e810b50e3d50ffd0081d355b5
zenith110 <thequake@live.com>
2021-10-17 18:30:27 -0400

    Removed reference to old stats page

--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
-import { StatsInterface } from './statsParent';
-interface StatsProps {
-  data: StatsInterface;
+import React, { useState } from 'react';
+interface StatsInterface {
+  hackers: number;
+  sponsors: number;
+  total: number;
 }
-export default function Stats({ data }: StatsProps): JSX.Element {
+export default function StatsComponent(): JSX.Element {
+  const [stats, setStats] = useState<StatsInterface>(Object);
+  const statsURL = 'https://api.knighthacks.org/api​/stats​/user_count​/';
+  fetch(statsURL, {
+    method: 'GET',
+    credentials: 'include',
+  })
+    .then((response) => response.json())
+    .then((data) => setStats(data))
+    .catch((err) => {
+      throw new Error(err);
+    });
+  console.log(stats);
+  console.log(stats.hackers);
   return (
     <div
       style={{
-        color: 'black',
-        borderStyle: 'solid',
-        backgroundColor: data.colorOfCard,
-        border: '10px black',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
-      <p>{data.statsName}</p>
-      <p>{data.statsNumber}</p>
+      {/* {dummyData.map((stat) => (
+        <Stats key={stat.statsName} data={stat} />
+      ))} */}
     </div>
   );
 }
--- src/pages/HackerPage.tsx
+++ src/pages/HackerPage.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import HackerTable from '../components/hackerTable';
-import StatsParent from '../components/statsParent';
+import StatsComponent from '../components/Stats';
 export default function HackerPage(): JSX.Element {
   return (
     <div>
       <p>Welcome to the hacker page!</p>
       <HackerTable />
-      <StatsParent />
+      <StatsComponent />
     </div>
   );
 }
