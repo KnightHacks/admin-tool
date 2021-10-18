9606096f861537482f1ef715c40eed90c05d1063
Abrahan N <35606009+zenith110@users.noreply.github.com>
2021-10-18 11:39:32 -0400

    Component for stats (#7)
    * Made template for stats page
    
    * Modified statsparent.tsx to use the api data
    
    * Removed reference to old stats page
    
    * Changed stats to be statscomponent.tsx
    
    * Fixed url for get stats
    
    * Added stats rendering
    
    * Optimizing fetch call for stats
    
    * Made logout button red

---
+++ src/components/StatsComponent.tsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+interface StatsInterface {
+  hackers: number;
+  sponsors: number;
+  total: number;
+}
+export default function StatsComponent(): JSX.Element {
+  const [stats, setStats] = useState<StatsInterface>(Object);
+  const statsURL = 'https://api.knighthacks.org/api/stats/user_count/';
+  useEffect(() => {
+    fetch(statsURL, {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setStats(data))
+      .catch((err) => {
+        throw new Error(err);
+      });
+  }, []);
+  return (
+    <div>
+      <p>Total hackers: {stats.hackers}</p>
+    </div>
+  );
+}
--- src/pages/HackerPage.tsx
+++ src/pages/HackerPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HackerTable from '../components/hackerTable';
+import StatsComponent from '../components/StatsComponent';
 export default function HackerPage(): JSX.Element {
   return (
     <div>
       <p>Welcome to the hacker page!</p>
       <HackerTable />
+      <StatsComponent />
     </div>
   );
 }
