4a32f0be90d50280934a1d2fb3952ad2f28ee625
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-10-22 19:40:39 -0400

    added basic formatting

--- src/components/hacker.tsx
+++ src/components/hacker.tsx
@@ -19,11 +19,27 @@ interface HackerRenderProps {
 export default function HackerRender({ data }: HackerRenderProps): JSX.Element {
   const [hackerText, setHackerText] = useState('');
 
+  function showAcceptReject(isaccepted: boolean) {
+    if (!isaccepted) {
+      return (
+        <div className="flex gap-2 mt-4 justify-end items-center">
+          <button className="py-3 px-8 bg-blue-400 rounded-lg font-bold">
+            {' '}
+            Accept{' '}
+          </button>
+          <button className="py-3 px-8 border-2 text-blue-900 border-blue-900 rounded-lg font-bold">
+            {' '}
+            Reject{' '}
+          </button>
+        </div>
+      );
+    }
+  }
   function showStatus(status: boolean, accepted: boolean) {
     if (!accepted) {
       return (
-        <div className="w-full flex align-center justify-end px-5">
-          <div className="flex h-1/2 align-center justify-center text-center rounded-xl py-3 px-8 font-medium italic text-red-900 bg-red-300 ">
+        <div className="w-full flex align-center justify-end md:px-5">
+          <div className="flex h-1/2 align-center w-36 justify-center text-center rounded-xl py-3 px-4 md:px-8 font-xs font-bold italic text-red-900 bg-red-300 ">
             Review{' '}
           </div>
         </div>
@@ -31,8 +47,8 @@ export default function HackerRender({ data }: HackerRenderProps): JSX.Element {
     }
     if (status) {
       return (
-        <div className="w-full flex align-center justify-end px-5">
-          <div className="flex h-1/2 align-center justify-center text-center rounded-xl py-3 px-8 font-medium italic text-green-900 bg-green-300 ">
+        <div className="w-full flex align-center justify-end md:px-5">
+          <div className="flex h-1/2 align-center  w-36 justify-center text-center rounded-xl py-3 px-4 md:px-8 font-xs  italic font-bold text-green-900 bg-green-300 ">
             {' '}
             Confirmed{' '}
           </div>
@@ -40,13 +56,16 @@ export default function HackerRender({ data }: HackerRenderProps): JSX.Element {
       );
     }
     return (
-      <div className="w-full flex align-center justify-end px-5">
-        <div className="flex h-1/2 align-center justify-center text-center rounded-xl py-3 px-8 font-medium italic text-yellow-900 bg-yellow-300 backdrop-opacity-25">
+      <div className="w-full flex align-center justify-end md:px-5">
+        <div className="flex h-1/2 align-center  w-36 justify-center text-center rounded-xl py-3 px-4 md:px-8 font-xs  italic font-bold text-yellow-900 bg-yellow-300 backdrop-opacity-25">
           {' '}
           Pending{' '}
         </div>
       </div>
     );
+  }
+  function showResume(email: string) {
+    console.log('show_resume');
   }
   function acceptHacker(data: Hacker, email: string): void {
     if (data.isaccepted === false) {
@@ -82,7 +101,7 @@ export default function HackerRender({ data }: HackerRenderProps): JSX.Element {
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <div className="h-24 px-4 grid grid-cols-2 w-full items-center justify-center">
+          <div className="h-24 px-4 grid grid-cols-2 w-full gap-3 items-center justify-center">
             <div className="flex flex-col  ">
               <text className="font-bold text-2xl">
                 {' '}
@@ -96,11 +115,41 @@ export default function HackerRender({ data }: HackerRenderProps): JSX.Element {
           </div>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            <p>Beginner: {data.beginner ? 'Yes' : 'No'}</p>
-            <p>Email: {data.email}</p>
-          </Typography>
-          <div style={{ margin: 10 }}>
+          <div className="md:grid md:grid-cols-2 flex flex-col px-5 gap-2 md:gap-0 mb-5">
+            <div className="grid grid-cols-2 justify-start gap-2">
+              <div className="font-bold"> Begineer </div>
+              <text> {data.beginner ? 'Yes' : 'No'} </text>
+
+              <div className="font-bold">Email </div>
+              <a href="mailto:user@gmail.com"> {data.email} </a>
+
+              <div className="font-bold"> GitHub </div>
+              <a href="github.com"> github.com/John-Doe </a>
+
+              <div className="font-bold"> LinkedIn </div>
+              <a href="linkedin.com"> linkedin.com/John-Doe </a>
+            </div>
+
+            <div className="grid grid-cols-2 justify-start gap-2">
+              <div className="font-bold"> More Information </div>
+              <text> Info </text>
+
+              <div className="font-bold"> More Information </div>
+              <a href="mailto:user@gmail.com"> Info </a>
+
+              <div className="font-bold">More Information </div>
+              <a href="github.com"> Info </a>
+
+              <div className="font-bold"> More Information </div>
+              <a href="linkedin.com"> Info </a>
+            </div>
+          </div>
+          {/* <iframe
+            src={`//api.knighthacks.org/api/hackers/${data.email}/resume/`}
+          /> */}
+          {showAcceptReject(data.isaccepted)}
+
+          {/* <div style={{ margin: 10 }}>
             <button
               className="button is-success"
               style={{ marginRight: 5 }}
@@ -113,7 +162,7 @@ export default function HackerRender({ data }: HackerRenderProps): JSX.Element {
           <iframe
             src={`//api.knighthacks.org/api/hackers/${data.email}/resume/`}
           />
-          <p>{hackerText}</p>
+          <p>{hackerText}</p> */}
         </AccordionDetails>
       </Accordion>
     </div>
