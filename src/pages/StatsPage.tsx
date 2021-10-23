087416b1d2485554a840636039c6b0e71c5e11f4
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-10-23 00:03:35 -0400

    added styling to stats page

--- src/pages/StatsPage.tsx
+++ src/pages/StatsPage.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
-
+import StatsComponent from '../components/StatsComponent';
 export default function StatsPage(): JSX.Element {
-  return <div />;
+  return (
+    <div>
+      <StatsComponent />
+    </div>
+  );
 }
