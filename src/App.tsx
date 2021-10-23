6d612d0b5666b1595232c17a1b7434b02aac0b97
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-10-22 23:46:51 -0400

    added stats page to the router

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import LoginPage from './pages/Login';
 import HackerPage from './pages/HackerPage';
+import StatsPage from './pages/StatsPage';
 function App(): JSX.Element {
   return (
     <div className="bg-gray-800 w-screen h-screen flex justify-center items-center">
@@ -10,6 +11,9 @@ function App(): JSX.Element {
           <Route exact path="/" component={LoginPage} />
           <Route path="/Hackers">
             <HackerPage />
+          </Route>
+          <Route path="/Stats">
+            <StatsPage />
           </Route>
         </Switch>
       </BrowserRouter>
---
+++ src/pages/StatsPage.tsx
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+
+export default function StatsPage(): JSX.Element {
+  return <div />;
+}
