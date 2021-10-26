d2d6da49a466169f9355d71926712d6814523773
zenith110 <thequake@live.com>
2021-10-25 20:11:48 -0400

    Removed echo to signify successful login

--- src/pages/HackerPage.tsx
+++ src/pages/HackerPage.tsx
@@ -2,26 +2,10 @@ import React, { useState, useEffect } from 'react';
 import HackerTable from '../components/hackerTable';
 import StatsComponent from '../components/StatsComponent';
 import SponsorsComponent from '../components/SponsorsComponent';
-import { useEndpoint } from '../api';
 
 export default function HackerPage(): JSX.Element {
-  const data = useEndpoint('http://localhost:5000/api/echo', { method: 'GET' });
-  const [json, setJson] = useState(null);
-
-  useEffect(() => {
-    if (data !== null) {
-      data.json().then((d) => setJson(d));
-    }
-  }, [data]);
-
-  if (json === null) {
-    return <p>Loading....</p>;
-  }
-
   return (
     <div>
-      <p>Welcome to the hacker page!</p>
-      <p>{JSON.stringify(json)}</p>
       <HackerTable />
       <StatsComponent />
       <SponsorsComponent />
