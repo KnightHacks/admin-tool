6df87aeff3c1e55aadcbb4e7780b3050b1d8de75
suneettipirneni <suneettipirneni@icloud.com>
2021-10-25 21:50:48 -0400

    fix: env var types

---
+++ src/@types/node/index.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    REACT_AZURE_CLIENT_ID: string;
+    REACT_AZURE_AUTHORITY: string;
+    REACT_AZURE_REDIRECT_URI: string;
+    REACT_AZURE_SCOPES: string;
+  }
+}
