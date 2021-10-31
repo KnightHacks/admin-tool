8804133d2fd547b9294d8035c2d1ef75071a1a76
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-10-31 15:18:10 -0400

    Removed email verification status

--- src/components/hacker.tsx
+++ src/components/hacker.tsx
@@ -52,16 +52,6 @@ export default function HackerRender({ data }: HackerRenderProps): JSX.Element {
     }
   }
   function showStatus(verified: boolean, accepted: boolean) {
-    if (!verified) {
-      return (
-        <div className="w-full flex align-center justify-end md:px-5">
-          <div className="flex h-1/2 align-center  w-36 justify-center text-center rounded-xl py-3 px-4 md:px-8 font-xs font-bold text-red-900 bg-red-300 backdrop-opacity-25">
-            {' '}
-            Unverified{' '}
-          </div>
-        </div>
-      );
-    }
     if (!accepted) {
       return (
         <div className="w-full flex align-center justify-end md:px-5">
