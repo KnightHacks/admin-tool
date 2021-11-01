b410ac84e5d3fcdb03d0386c8e0438baa89aba7b
zenith110 <thequake@live.com>
2021-11-01 08:30:45 -0400

    Fixed some redirecting issues

--- src/pages/HackerPage.tsx
+++ src/pages/HackerPage.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import HackerTable from '../components/hackerTable';
-import StatsComponent from '../components/StatsComponent';
-import SponsorsComponent from '../components/SponsorsComponent';
 
 export default function HackerPage(): JSX.Element {
   return (
     <div className="overflow-y-auto">
       <HackerTable />
-      <StatsComponent />
-      <SponsorsComponent />
     </div>
   );
 }
