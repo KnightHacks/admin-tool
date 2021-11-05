f75f87c6c92d46397e355f090e78ba30db516991
Abrahan N <35606009+zenith110@users.noreply.github.com>
2021-11-05 17:47:00 -0400

    Commented out csv
    Commented out csv data for now

--- src/components/hackers/hackerTable.tsx
+++ src/components/hackers/hackerTable.tsx
@@ -39,29 +39,29 @@ export default function HackerTable(): JSX.Element {
     }
   });
 
-  const options = {
-    fieldSeparator: ',',
-    quoteStrings: '"',
-    decimalSeparator: '.',
-    showLabels: true,
-    showTitle: true,
-    title: 'Knight Hacks 2021 Attendee Data',
-    useTextFile: false,
-    useBom: true,
-    useKeysAsHeaders: true,
-    filename: 'KH_2021',
-    // headers: ['Column 1', 'Column 2', etc...] <-- Won't work with useKeysAsHeaders present!
-  };
+//   const options = {
+//     fieldSeparator: ',',
+//     quoteStrings: '"',
+//     decimalSeparator: '.',
+//     showLabels: true,
+//     showTitle: true,
+//     title: 'Knight Hacks 2021 Attendee Data',
+//     useTextFile: false,
+//     useBom: true,
+//     useKeysAsHeaders: true,
+//     filename: 'KH_2021',
+//     // headers: ['Column 1', 'Column 2', etc...] <-- Won't work with useKeysAsHeaders present!
+//   };
 
-  const csvExporter = new ExportToCsv(options);
+//   const csvExporter = new ExportToCsv(options);
 
   return (
     <div className="h-full w-full mb-12 flex flex-col items-center justify-center gap-3">
       <div className="h-20  w-full flex flex-row-reverse mx-8 items-center justify-start">
-        <button
+<!--         <button
           className="mx-5 bg-transparent hover:bg-yellow-400 text-yellow-400 font-semibold hover:text-white py-2 px-4 border border-yellow-400 hover:border-transparent rounded"
           onClick={() => csvExporter.generateCsv(hackers)}
-        >
+        > -->
           {' '}
           Export{' '}
         </button>
